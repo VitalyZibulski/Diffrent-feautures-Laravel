a8e44480c8c32f3720a5dd0c117dcbd039b2b223
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-07-14 10:44:07 +0300

    add template, controller, js

--- 1.Live search using Ajax/app/Http/Controllers/LiveSearch.php
+++ 1.Live search using Ajax/app/Http/Controllers/LiveSearch.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Customer;
 
 class LiveSearch extends Controller
 {
     function index()
 	{
 		return view('live_search');
+	}
+
+	function getData(Request $request)
+	{
+
 	}
 }
--- 1.Live search using Ajax/routes/web.php
+++ 1.Live search using Ajax/routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/getdata', 'LiveSearch@getData')->name('live-search-getData');
